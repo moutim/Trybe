44d4d8114c23405cc549db990e8862ce5e17d20c
Vitor Moutim <moutimg@gmail.com>
2021-11-08 15:57:36 -0300

    Questao 1 - exercicio do dia

---
+++ fundamentos/bloco-4-javascript/js-dia2/exercicioDoDia.js
@@ -0,0 +1,8 @@
+// Leia atentamente os enunciados e faça o que se pede! Iremos utilizar esse array para realizar os exercícios do 1 ao 7:
+let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+
+// 1 - Nesse primeiro exercício, percorra o array imprimindo todos os valores nele contidos com a função console.log():
+for(let number of numbers){
+    console.log(number);
+}
+
