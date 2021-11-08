81431da7caadbbebb68101699a2e89a6a18a8522
Vitor Moutim <moutimg@gmail.com>
2021-11-08 16:47:11 -0300

    Questao 2 - exercicio do dia

--- fundamentos/bloco-4-javascript/js-dia2/exercicioDoDia.js
+++ fundamentos/bloco-4-javascript/js-dia2/exercicioDoDia.js
@@ -2,7 +2,16 @@
 let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
 
 // 1 - Nesse primeiro exercício, percorra o array imprimindo todos os valores nele contidos com a função console.log():
-for(let number of numbers){
-    console.log(number);
+// for(let number of numbers){
+//     console.log(number);
+// }
+
+// 2 - Para o segundo exercício, some todos os valores contidos no array e imprima o resultado:
+// Pra resolver esse exercicio tive que ver esse codigo para me situar na questao. Entendi o que foi escrito e apliquei no exercicio
+// https://www.horadecodar.com.br/2021/01/18/como-somar-elementos-de-um-array-de-maneira-performaica/
+let soma = 0;
+for(var i = 0; i < numbers.length; i++) {
+    soma += numbers[i];
 }
+console.log(soma);
 
