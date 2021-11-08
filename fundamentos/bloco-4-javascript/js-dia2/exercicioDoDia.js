99b4fbde3d448048289ae7047c3b8804c4660f12
Vitor Moutim <moutimg@gmail.com>
2021-11-08 17:10:34 -0300

    Questao 3 - exercicio do dia

--- fundamentos/bloco-4-javascript/js-dia2/exercicioDoDia.js
+++ fundamentos/bloco-4-javascript/js-dia2/exercicioDoDia.js
@@ -2,9 +2,9 @@
 let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
 
 // 1 - Nesse primeiro exercício, percorra o array imprimindo todos os valores nele contidos com a função console.log():
-// for(let number of numbers){
-//     console.log(number);
-// }
+for(let number of numbers){
+    console.log(number);
+}
 
 // 2 - Para o segundo exercício, some todos os valores contidos no array e imprima o resultado:
 // Pra resolver esse exercicio tive que ver esse codigo para me situar na questao. Entendi o que foi escrito e apliquei no exercicio
@@ -15,3 +15,11 @@ for(var i = 0; i < numbers.length; i++) {
 }
 console.log(soma);
 
+
+
+// 3 - Para o terceiro exercício, calcule e imprima a média aritmética dos valores contidos no array:
+let media = 0;
+for(var i = 0; i < numbers.length; i++) {
+    media += numbers[i];
+}
+console.log(media / numbers.length );
