2eb45e7cf7e917ab9d6f321fcba684e9356a107a
Vitor Moutim <moutimg@gmail.com>
2021-11-09 20:20:32 -0300

    Questao 2 - exercicio do dia

--- fundamentos/bloco-4-javascript/js-dia3/exercicioDoDia.js
+++ fundamentos/bloco-4-javascript/js-dia3/exercicioDoDia.js
@@ -3,3 +3,13 @@ let valor = 5;
 for (let i = 0; i < valor; i += 1){
         console.log('*****');
 }
+
+
+
+// 2- Para o segundo exercício, faça o mesmo que antes, mas que imprima um triângulo retângulo com 5 asteriscos de base. 
+let simbolo = '*';
+let vazio = '';
+for (let i = 0; i < valor; i += 1){
+        vazio = vazio + simbolo
+        console.log(vazio);
+}
