fe3aeb412d4b6b9d07303cace5091c534f934f3a
Vitor Moutim <moutimg@gmail.com>
2021-11-09 19:23:39 -0300

    Questao 1 - exercicio do dia

---
+++ fundamentos/bloco-4-javascript/js-dia3/exercicioDoDia.js
@@ -0,0 +1,5 @@
+// 1- Para o primeiro exercício de hoje, faça um programa que, dado um valor n qualquer, seja n > 1 , imprima na tela um quadrado feito de asteriscos de lado de tamanho n.
+let valor = 5;
+for (let i = 0; i < valor; i += 1){
+        console.log('*****');
+}
