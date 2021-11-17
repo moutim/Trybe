2486c225d2b066829090f1c8ab6d7b9fd7567a26
Vitor Moutim <moutimg@gmail.com>
2021-11-17 14:59:23 -0300

    Exercicios da Parte 1 do conteudo

---
+++ fundamentos/bloco-5-js-dom-eventos-webstorage/js-dom-dia2/buscarElementos.js
@@ -0,0 +1,24 @@
+// 1 - Acesse o elemento elementoOndeVoceEsta .
+// 2 - Acesse pai a partir de elementoOndeVoceEsta e adicione uma color a ele.
+// 3 - Acesse o primeiroFilhoDoFilho e adicione um texto a ele. Você se lembra dos vídeos da aula anterior, como fazer isso?
+// 4 - Acesse o primeiroFilho a partir de pai .
+// 5 - Agora acesse o primeiroFilho a partir de elementoOndeVoceEsta .
+// 6 - Agora acesse o texto Atenção! a partir de elementoOndeVoceEsta .
+// 7 - Agora acesse o terceiroFilho a partir de elementoOndeVoceEsta .
+// 8 - Agora acesse o terceiroFilho a partir de pai .
+
+console.log(document.getElementById('elementoOndeVoceEsta'));
+
+console.log(document.getElementById('elementoOndeVoceEsta').parentNode);
+
+document.getElementById('primeiroFilhoDoFilho').innerText = 'Texto generico';
+
+console.log(document.getElementById('pai').firstElementChild);
+
+console.log(document.getElementById('elementoOndeVoceEsta').previousElementSibling);
+
+console.log(document.getElementById('elementoOndeVoceEsta').nextSibling);
+
+console.log(document.getElementById('elementoOndeVoceEsta').nextElementSibling);
+
+console.log(document.getElementById('pai').lastElementChild.previousElementSibling);
