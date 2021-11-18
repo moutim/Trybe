3c8fc4fe2772d66d3665c9b57b8db1c0af0b7121
Vitor Moutim <moutimg@gmail.com>
2021-11-18 19:19:20 -0300

    3 - exercicio do dia

--- fundamentos/bloco-5-js-dom-eventos-webstorage/js-dom-dia3/main.js
+++ fundamentos/bloco-5-js-dom-eventos-webstorage/js-dom-dia3/main.js
@@ -37,6 +37,10 @@ input.addEventListener('input', alteraTexto);
 // 4. Crie uma função que, ao clicar duas vezes em 'Meu top 3 do Spotrybefy', ele
 // redirecione para alguma página;
 // 4.1. Que tal redirecionar para seu portifólio?
+function redirecionaPagina(){
+    window.open('https://moutim.github.io/', '_blank');
+}
+myWebpage.addEventListener('dblclick', redirecionaPagina);
 
 // 5. Crie uma função que, ao passar o mouse sobre 'Meu top 3 do Spotrybefy', altere
 // a cor do mesmo;
