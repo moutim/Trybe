8ba5725387f3891c08cb1570e99952018dfaa6bb
Vitor Moutim <moutimg@gmail.com>
2021-11-21 18:16:34 -0300

    funcoes terminadas

--- fundamentos/bloco-5-js-dom-eventos-webstorage/js-webstorage-dia4/script.js
+++ fundamentos/bloco-5-js-dom-eventos-webstorage/js-webstorage-dia4/script.js
@@ -2,11 +2,13 @@ let inputBackground = document.getElementById('backgroundColor');
 let inputFontColor = document.getElementById('fontColor');
 let inputFontSize = document.getElementById('fontSize');
 let inputFontStyle = document.getElementById('fontStyle');
+let inputLineHeight = document.getElementById('lineHeight');
 
 let btnBackground = document.getElementById('btn-background');
 let btnFontColor = document.getElementById('btn-fontColor');
 let btnFontSize = document.getElementById('btn-fontSize');
 let btnFontStyle = document.getElementById('btn-fontStyle');
+let btnLineHeight = document.getElementById('btn-lineHeight');
 
 let body = document.getElementsByTagName('body')[0];
 let p = document.getElementsByTagName('p');
@@ -42,7 +44,35 @@ function fontStyle () {
 }
 btnFontStyle.addEventListener('click', fontStyle);
 
+function lineHeight () {
+    let tamanho = inputLineHeight.value;
+    for (let i of p){
+        i.style.lineHeight = tamanho;
+    }
+    localStorage.setItem('lineHeight', tamanho);
+}
+btnLineHeight.addEventListener('click', lineHeight);
 
 window.onload = function () {
+    function loadUserOptions () {
+        let userBackground = localStorage.getItem('backgroundColor');
+        body.style.backgroundColor = userBackground;
+
+        let userFontColor = localStorage.getItem('fontColor');
+        body.style.color = userFontColor;
+
+        let userFontSize = localStorage.getItem('fontSize');
+        body.style.fontSize = userFontSize;
+
+        let userFontStyle = localStorage.getItem('fontStyle');
+        let userLineHeight = localStorage.getItem('lineHeight');
+        for (let i of p) {
+            i.style.fontFamily = userFontStyle;
+            i.style.lineHeight = userLineHeight;
+        }
+
+        
+    }
+    loadUserOptions();
 
 }
