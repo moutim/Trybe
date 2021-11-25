bd9687d7c53b80ee86df61aaaf1fba8d4ce8aee6
Vitor Moutim <moutimg@gmail.com>
2021-11-25 15:56:37 -0300

    Exercicio do dia 1 - Crie um <fieldset> para dados pessoais

---
+++ fundamentos/bloco-6-forms-flexbox-responsivo/forms-dia1/script.js
@@ -0,0 +1,41 @@
+const estados = {   
+'AC': 'Acre',
+'AL': 'Alagoas',
+'AP': 'Amapá',
+'AM': 'Amazonas',
+'BA': 'Bahia',
+'CE': 'Ceará',
+'DF': 'Distrito Federal',
+'ES': 'Espírito Santo',
+'GO': 'Goías',
+'MA': 'Maranhão',
+'MT': 'Mato Grosso',
+'MS': 'Mato Grosso do Sul',
+'MG': 'Minas Gerais',
+'PA': 'Pará',
+'PB': 'Paraíba',
+'PR': 'Paraná',
+'PE': 'Pernambuco',
+'PI': 'Piauí',
+'RJ': 'Rio de Janeiro',
+'RN': 'Rio Grande do Norte',
+'RS': 'Rio Grande do Sul',
+'RO': 'Rondônia',
+'RR': 'Roraíma',
+'SC': 'Santa Catarina',
+'SP': 'São Paulo',
+'SE': 'Sergipe',
+'TO': 'Tocantins'
+}
+
+const select = document.getElementById('estado');
+function criaOptions () {
+    for (let i in estados) {
+        const option = document.createElement('option');
+        option.setAttribute('value', i);
+        option.setAttribute('required', '');
+        option.innerText = estados[i];
+        select.appendChild(option);
+    }
+}
+criaOptions();
