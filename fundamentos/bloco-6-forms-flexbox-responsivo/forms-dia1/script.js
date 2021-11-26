c42eef267385c636fad5a73dd358a2fdcac8c203
Vitor Moutim <moutimg@gmail.com>
2021-11-26 13:36:35 -0300

    Exercicio do dia 3 - Exibir as informacoes na tela, falta terminar de organizar os resultados

--- fundamentos/bloco-6-forms-flexbox-responsivo/forms-dia1/script.js
+++ fundamentos/bloco-6-forms-flexbox-responsivo/forms-dia1/script.js
@@ -1,3 +1,4 @@
+// Funcao para criar options do select
 const estados = {   
 'AC': 'Acre',
 'AL': 'Alagoas',
@@ -27,7 +28,6 @@ const estados = {
 'SE': 'Sergipe',
 'TO': 'Tocantins'
 }
-
 const select = document.getElementById('estado');
 function criaOptions () {
     for (let i in estados) {
@@ -39,6 +39,7 @@ function criaOptions () {
 }
 criaOptions();
 
+// Funcao para verificar data 
 const data = document.getElementById('data');
 function verificaData () {
     const valorData = data.value;
@@ -55,5 +56,52 @@ function verificaData () {
     return alert('Insira uma data valida!')
 }
 
+// Funcao enviar formulario
+const botao = document.getElementById('enviar');
+const div = document.createElement('div');
 
+function valorInputs() {
+    const inputs = document.getElementsByTagName('input');
+    for (let i of inputs) {
+        const p = document.createElement('p');
+        let name = i.name;
+        let value = i.value;
+        p.innerHTML = `<strong>${name}</strong>: ${value}`;
+        div.appendChild(p);
+    }
+}
+function valorOption () {
+    const select = document.getElementById('estado');
+    let optionValor = select.options[select.selectedIndex].value;
+    let p = document.createElement('p');
+    p.innerText = `Estado: ${optionValor}`;
+    div.appendChild(p);
+}
+function valorRadio () {
+    const radio = document.getElementsByName('tipo');
+    for (let i = 0; i < radio.length; i += 1) {
+        if (radio[i].checked) {
+            let p = document.createElement('p');
+            p.innerText = `Mora em: ${radio[i].value}`;
+            div.appendChild(p);
+        }
+    }
+}
+const infos = document.getElementById('secao-info');
+function acionaFormulario (event) {
+    event.preventDefault();
+    verificaData();
+    valorInputs();
+    valorOption();
+    valorRadio();
+
+    const textArea = document.getElementsByTagName('textarea');
+    let textValor = textArea.innerText;
+    let p = document.createElement('p');
+    p.innerText = textValor;
+    div.appendChild(p);
+
+    infos.appendChild(div);
+}
+botao.addEventListener('click', acionaFormulario);
 
