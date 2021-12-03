bd8d6a3f31e17b777783d9a0bf3a010dc8bc99f8
Vitor Moutim <moutimg@gmail.com>
2021-12-03 18:28:57 -0300

    muda as string para template literals

--- fundamentos/bloco-7-javascript-es6/es6-dia1/exercicioDoDia1.js
+++ fundamentos/bloco-7-javascript-es6/es6-dia1/exercicioDoDia1.js
@@ -1,11 +1,11 @@
 
 const testingScope = escopo => {
     if (escopo === true) {
-      let ifScope = 'Não devo ser utilizada fora do meu escopo (if)';
-      ifScope = ifScope + ' ótimo, fui utilizada no escopo !';
+      let ifScope = `Não devo ser utilizada fora do meu escopo (if)`;
+      ifScope = `${ifScope}. ótimo, fui utilizada no escopo !`;
       console.log(ifScope);
     } else {
-      let elseScope = 'Não devo ser utilizada fora meu escopo (else)';
+      let elseScope = `Não devo ser utilizada fora meu escopo (else)`;
       console.log(elseScope);
     }
 }
