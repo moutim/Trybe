1ae2c273da78b53729e20635387ad6afde40b617
Vitor Moutim <moutimg@gmail.com>
2021-12-06 18:56:40 -0300

    5 - Exercicio do dia pt2

--- fundamentos/bloco-7-javascript-es6/es6-dia2/exercicioDoDia2.js
+++ fundamentos/bloco-7-javascript-es6/es6-dia2/exercicioDoDia2.js
@@ -43,3 +43,6 @@ function listaValores (objeto) {
 }
 listaValores(lesson1);
 
+// 5 - Crie um objeto de nome allLessons , que deve agrupar todas as aulas através do Object.assign . Cada chave desse novo objeto será uma aula, sendo essas chaves: lesson1 , lesson2 e lesson3 . Ao executar o comando console.log(allLessons) , a saída deverá ser a seguinte:
+const allLessons = Object.assign({}, {lesson1, lesson2, lesson3});
+console.log(allLessons);
