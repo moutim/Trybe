7d298d1cfa11cb6c87d71f0ae3687af670ea9bed
Vitor Moutim <moutimg@gmail.com>
2021-12-06 18:25:41 -0300

    1 - Exercicio do dia pt1

---
+++ fundamentos/bloco-7-javascript-es6/es6-dia2/exercicioDoDia1.js
@@ -0,0 +1,52 @@
+const order = {
+    name: 'Rafael Andrade',
+    phoneNumber: '11-98763-1416',
+    address: {
+      street: 'Rua das Flores',
+      number: '389',
+      apartment: '701',
+    },
+    order: {
+      pizza: {
+        marguerita: {
+          amount: 1,
+          price: 25,
+        },
+        pepperoni: {
+          amount: 1,
+          price: 20,
+        }
+      },
+      drinks: {
+        coke: {
+          type: 'Coca-Cola Zero',
+          price: 10,
+          amount: 1,
+        }
+      },
+      delivery: {
+        deliveryPerson: 'Ana Silveira',
+        price: 5,
+      }
+    },
+    payment: {
+      total: 60,
+    },
+  };
+  
+// 1 - Complete a função customerInfo() para que seu retorno seja similar a "Olá Ana Silveira, entrega para: Rafael Andrade, Telefone: 11-98763-1416, R. Rua das Flores, Nº: 389, AP: 701". 
+  const customerInfo = (order) => {
+      const deliveryPerson = order.order.delivery.deliveryPerson;
+      const customerName = order.name;
+      const customerPhone = order.phoneNumber; 
+      const adress = Object.values(order.address);
+      return console.log(`Olá ${deliveryPerson}, entrega para: ${customerName}, Telefone: ${customerPhone}, Rua: ${adress[0]}, Nº: ${adress[1]}, AP: ${adress[2]}`);
+  }
+  customerInfo(order);
+  
+  const orderModifier = (order) => {
+    // Adicione abaixo as informações necessárias.
+  
+  }
+  
+  orderModifier(order);
