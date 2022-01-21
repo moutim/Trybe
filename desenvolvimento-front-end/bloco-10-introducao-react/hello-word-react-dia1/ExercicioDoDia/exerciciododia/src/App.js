7290089ed40249a349b38cf0a53cce8a9e0ae1c3
Vitor <moutimg@gmail.com>
2022-01-21 18:59:17 -0300

    Criando lista de tarefas e adicionando a tela

--- desenvolvimento-front-end/bloco-10-introducao-react/hello-word-react-dia1/ExercicioDoDia/exerciciododia/src/App.js
+++ desenvolvimento-front-end/bloco-10-introducao-react/hello-word-react-dia1/ExercicioDoDia/exerciciododia/src/App.js
@@ -1,23 +1,23 @@
 import logo from './logo.svg';
 import './App.css';
 
+// Crie uma lista de tarefas simples seguindo os passos abaixo:
+// insira a função Task a seguir acima do seu componente App dentro do arquivo App.js :
+const tasks = ['Levar o cachorro pra passear;', 'Ir ao mercado;', 'Lavar roupas', 'Lavar o quintal', 'Fazer os exercicios'];
+
+const Task = (value) => {
+  return (
+    <li>{value}</li>
+  );
+}
+
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <ol>
+        <h1>Lista de tarefas</h1>
+        { tasks.map((item) => Task(item)) }
+      </ol>
     </div>
   );
 }
