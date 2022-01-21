6c84bcb6763f584ff913afa6ec4ce8bbfdf9d328
Vitor <moutimg@gmail.com>
2022-01-21 18:35:10 -0300

    Exercicio de fixacao

---
+++ desenvolvimento-front-end/bloco-10-introducao-react/hello-word-react-dia1/React/parafixar/src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+import AboutMe from './Component';
+
+
+function App() {
+  return (
+      <AboutMe></AboutMe>
+  );
+}
+
+export default App;
---
+++ desenvolvimento-front-end/bloco-10-introducao-react/hello-word-react-dia1/React/parafixar/src/Component.jsx
@@ -0,0 +1,14 @@
+import React  from "react";
+
+class AboutMe extends React.Component {
+    render(){
+        return(
+            <div>
+                <h1>Vitor Moutim</h1>
+                <p>Moro em Sao Paulo e atualmente estou em formacao pela Trybe</p>
+            </div>
+        )
+    }
+}
+
+export default AboutMe;
