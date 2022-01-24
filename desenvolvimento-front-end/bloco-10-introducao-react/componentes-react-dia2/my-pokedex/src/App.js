411dae559018937b96e09e0df26d09ced333dbf7
Vitor <moutimg@gmail.com>
2022-01-24 19:23:32 -0300

    Renderizando Pokedex na tela

--- desenvolvimento-front-end/bloco-10-introducao-react/componentes-react-dia2/my-pokedex/src/App.js
+++ desenvolvimento-front-end/bloco-10-introducao-react/componentes-react-dia2/my-pokedex/src/App.js
@@ -1,6 +1,13 @@
+import Pokedex from "./Pokedex";
+import data from './data';
+
 function App() {
   return (
     <div className="App">
+      <h1>Pokedex</h1>
+      <div className="pokemon-list">
+        <Pokedex listPokemon={data} />
+      </div>
     </div>
   );
 }
