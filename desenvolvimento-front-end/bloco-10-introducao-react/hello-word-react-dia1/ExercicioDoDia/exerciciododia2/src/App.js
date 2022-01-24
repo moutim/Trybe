28bf3b8ca6e2e33c716802d990d9a5a3f880f60f
Vitor <moutimg@gmail.com>
2022-01-24 14:33:04 -0300

    Renderiza footer na pagina

--- desenvolvimento-front-end/bloco-10-introducao-react/hello-word-react-dia1/ExercicioDoDia/exerciciododia2/src/App.js
+++ desenvolvimento-front-end/bloco-10-introducao-react/hello-word-react-dia1/ExercicioDoDia/exerciciododia2/src/App.js
@@ -1,11 +1,13 @@
 import Header from "./Header";
 import Content from "./Content";
+import Footer from "./footer";
 
 function App() {
   return (
     <div className="App">
       <Header></Header>
       <Content></Content>
+      <Footer></Footer>
     </div>
   );
 }
