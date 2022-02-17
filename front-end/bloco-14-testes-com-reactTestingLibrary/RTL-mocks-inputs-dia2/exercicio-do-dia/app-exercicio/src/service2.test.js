2609321a63fd686e3db02522928f777ef332d478
Vitor <moutimg@gmail.com>
2022-02-17 19:56:55 -0300

    Corrige erro de jest

--- front-end/bloco-14-testes-com-reactTestingLibrary/RTL-mocks-inputs-dia2/exercicio-do-dia/app-exercicio/src/service2.test.js
+++ front-end/bloco-14-testes-com-reactTestingLibrary/RTL-mocks-inputs-dia2/exercicio-do-dia/app-exercicio/src/service2.test.js
@@ -1,22 +1,23 @@
-import React from 'react';
 import { render, screen } from '@testing-library/react';
 import App from './App';
-const service = require('./service');
+import '@testing-library/jest-dom';
+// import userEvent from '@testing-library/user-event';
 
 describe('Testa a funcao fetchAPI retorna o objeto esperado', () => {
     it('Verifica o objeto retornado', async () => {
-        service.fetchAPI.mockRestore();
+        // service.fetchAPI.mockRestore();
         const objDog = {
             message: "https://images.dog.ceo/breeds/terrier-patterdale/Patterdale.jpg",
             status: "success"
         }
 
-        global.service.fetchAPI = jest.fn(async () => ({
+        global.fetch = jest.fn(async () => ({
             json: async () => objDog,
         }));
 
         render(<App />);
         const linkImg = await screen.findByAltText(objDog.message);
+        console.log(linkImg);
         expect(linkImg).toBeInTheDocument();
 
     })
