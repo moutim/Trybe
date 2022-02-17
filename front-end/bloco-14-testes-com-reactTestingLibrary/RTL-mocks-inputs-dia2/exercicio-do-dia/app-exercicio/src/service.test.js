947ac308eafe14ab4b1204286ed4c5fd2ab1aaea
Vitor <moutimg@gmail.com>
2022-02-16 21:28:12 -0300

    questao 3

--- front-end/bloco-14-testes-com-reactTestingLibrary/RTL-mocks-inputs-dia2/exercicio-do-dia/app-exercicio/src/service.test.js
+++ front-end/bloco-14-testes-com-reactTestingLibrary/RTL-mocks-inputs-dia2/exercicio-do-dia/app-exercicio/src/service.test.js
@@ -15,4 +15,23 @@ describe('Testa se a funcao executa como esperado', () => {
         expect(service.randomNumber).toHaveBeenCalledTimes(1);
         expect(service.randomNumber).toHaveBeenCalledWith(5, 5);
     });
+
+    it('Testa nova funcionalidade de multiplicacao da funcao', () => {
+        service.randomNumber = jest.fn().mockImplementation((a, b, c) => a * b * c);
+        expect(service.randomNumber(5, 5, 5)).toBe(125);
+        expect(service.randomNumber).toHaveBeenCalled();
+        expect(service.randomNumber).toHaveBeenCalledTimes(1);
+        expect(service.randomNumber).toHaveBeenCalledWith(5, 5, 5);
+    });
+
+    it('Testa nova funcionalidade de retornar o dobro de um numero', () => {
+        service.randomNumber.mockReset();
+        expect(service.randomNumber).toHaveBeenCalledTimes(0);
+
+        service.randomNumber = jest.fn().mockImplementationOnce(a => a * 2);
+        expect(service.randomNumber(5)).toBe(10);
+        expect(service.randomNumber).toHaveBeenCalled();
+        expect(service.randomNumber).toHaveBeenCalledTimes(1);
+        expect(service.randomNumber).toHaveBeenCalledWith(5);
+    })
 });
