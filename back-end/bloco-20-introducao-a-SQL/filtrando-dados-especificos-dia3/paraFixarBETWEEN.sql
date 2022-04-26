91bd28779f81188d62f6757a0a850a94c967087a
Vitor <moutimg@gmail.com>
2022-04-26 19:16:37 -0300

    Exercicios de fixacao usando BETWEEN

---
+++ back-end/bloco-20-introducao-a-SQL/filtrando-dados-especificos-dia3/paraFixarBETWEEN.sql
@@ -0,0 +1,20 @@
+USE sakila;
+
+SELECT first_name, last_name, email FROM customer
+WHERE last_name IN ('hicks', 'crawford', 'henry', 'boyd', 'mason', 'morales', 'kennedy')
+ORDER BY first_name;
+
+SELECT email FROM customer
+WHERE address_id BETWEEN 172 AND 176
+ORDER BY email;
+
+SELECT COUNT(*) FROM payment
+WHERE payment_date BETWEEN '2005-05-01' AND '2005-08-01';
+
+SELECT title, release_year, rental_duration FROM film
+WHERE rental_duration BETWEEN  3 AND 6
+ORDER BY rental_duration DESC, title ASC;
+
+SELECT title, rating FROM film
+WHERE rating IN('G', 'PG', 'PG-13')
+ORDER BY title LIMIT 500;
