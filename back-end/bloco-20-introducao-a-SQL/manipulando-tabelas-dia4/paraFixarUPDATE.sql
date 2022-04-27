6d0744185b95eae4b602eed5bf7104b0a7487f8b
Vitor <moutimg@gmail.com>
2022-04-27 16:05:24 -0300

    Exercicios de fixacao usando UPDATE

---
+++ back-end/bloco-20-introducao-a-SQL/manipulando-tabelas-dia4/paraFixarUPDATE.sql
@@ -0,0 +1,23 @@
+USE sakila;
+
+UPDATE actor 
+SET first_name = 'JULES'
+WHERE first_name = 'JULIA';
+
+UPDATE category
+SET name = 'Science Fiction'
+WHERE name = 'Sci-Fi';
+
+UPDATE film
+SET rental_rate = 25
+WHERE length > 100
+AND rating IN ('G', 'PG', 'PG-13')
+AND replacement_cost > 20;
+
+UPDATE film
+SET rental_rate = (
+	CASE
+		WHEN length BETWEEN 0 AND 100 THEN 10
+        WHEN length > 100 THEN 20
+	END
+);
